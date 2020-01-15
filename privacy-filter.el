35183cbb9ae3deaf8ceab37235f3aa2e0acd0ce1
riscy <riscy@users.noreply.github.com>
2020-01-14 20:48:41 -0800

    rewrite

--- privacy-filter.el
+++ privacy-filter.el
@@ -1,4 +1,4 @@
-;;; privacy-filter.el --- A digital "privacy filter" mode -*- lexical-binding: t -*-
+;;; privacy-filter.el --- A digital "privacy filter" mode -*- lexical-binding: t; eval: (privacy-filter-mode 1) -*-
 
 ;; Authors: Chris Rayner (dchrisrayner@gmail.com)
 ;; Created: 2020/01/12
@@ -25,6 +25,7 @@
 ;;; Code:
 
 (require 'color)
+(require 'face-remap)
 
 (defgroup privacy-filter nil
   "A digital 'privacy filter' mode."
@@ -49,46 +50,47 @@ Setting this to a smaller number makes private text harder to see."
 (defvar-local privacy-filter--font-lock-p nil
   "Whether font-lock-mode was on when the privacy filter was activated.")
 
+(defvar-local privacy-filter--cookie nil)
+
 ;;;###autoload
 (define-minor-mode privacy-filter-mode
   "Toggle privacy-filter-mode on or off."
   :lighter "p "
-  (if privacy-filter-mode (privacy-filter--on) (privacy-filter--off)))
+  (if privacy-filter-mode (privacy-filter--enable) (privacy-filter--disable)))
 
-(defun privacy-filter--on ()
+(defun privacy-filter--enable ()
   "Turn on the privacy filter (disable font-locks, overlays, etc.)."
-  (setq-local privacy-filter--font-lock-p font-lock-mode)
-  (font-lock-mode -1)
-  (privacy-filter--update-face)
-  (privacy-filter--remove-overlays)
-  (privacy-filter--propertize-text))
+  (privacy-filter--unfontify)
+  (privacy-filter--refontify))
 
-(defun privacy-filter--off ()
+(defun privacy-filter--disable ()
   "Turn off the privacy filter and resume font-locking."
-  (set-text-properties (point-min) (point-max) nil)
-  (when privacy-filter--font-lock-p (font-lock-mode 1)))
+  (let ((inhibit-read-only t))
+    (with-silent-modifications
+      ;; (set-text-properties (point-min) (point-max) nil)
+      (face-remap-remove-relative privacy-filter--cookie)
+      (when (or privacy-filter--font-lock-p global-font-lock-mode)
+        (font-lock-mode 1)))))
 
-(defun privacy-filter--update-face ()
-  "Update `privacy-filter-face' relative to the current background color."
-  (set-face-foreground
-   'privacy-filter-face
-   (if (eq (frame-parameter nil 'background-mode) 'dark)
-       (color-lighten-name
-        (face-attribute 'default :background) privacy-filter-amount)
-     (color-darken-name
-      (face-attribute 'default :background) privacy-filter-amount))))
-
-(defun privacy-filter--remove-overlays ()
-  "Remove additional fontification (i.e. overlays) from the buffer.
+(defun privacy-filter--unfontify ()
+  "Remove additional fontification from the buffer.
 Also, prevent overlays from coming back by advising `overlay-put'."
+  (let ((inhibit-read-only t))
+   (with-silent-modifications (set-text-properties (point-min) (point-max) nil)))
   (dolist (o (overlays-in (point-min) (point-max))) (delete-overlay o))
-  (advice-add #'overlay-put :around #'privacy-filter--hide-overlays))
+  (advice-add #'overlay-put :around #'privacy-filter--hide-overlays)
+  (setq-local privacy-filter--font-lock-p font-lock-mode)
+  (font-lock-mode -1))
 
-(defun privacy-filter--propertize-text ()
-  "Propertize all of the text in the buffer."
-  (with-silent-modifications
-    (set-text-properties (point-min) (point-max) nil)
-    (add-text-properties (point-min) (point-max) '(face privacy-filter-face))))
+(defun privacy-filter--refontify ()
+  "Update `privacy-filter-face' relative to the current background color."
+  (let ((background (face-attribute 'default :background)))
+    (set-face-foreground 'privacy-filter-face
+     (if (eq (frame-parameter nil 'background-mode) 'dark)
+         (color-lighten-name background privacy-filter-amount)
+       (color-darken-name background privacy-filter-amount))))
+  (setq-local privacy-filter--cookie
+              (face-remap-add-relative 'default 'privacy-filter-face)))
 
 (defun privacy-filter--hide-overlays (overlay-put-function &rest args)
   "Advice to block OVERLAY-PUT-FUNCTION (ARGS)."
